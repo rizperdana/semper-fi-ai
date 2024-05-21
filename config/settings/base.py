75462a26d089a527c178e1626ec9f411fd22575f
Rizki Perdana <perdana.rizki16@gmail.com>
2024-05-21 21:58:46 +0700

    setup nginx and finalize basic containerization

--- config/settings/base.py
+++ config/settings/base.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery
+CELERY_BROKER_URL = config("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = config("REDIS_BACKEND")
